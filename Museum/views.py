b6d1f490a623eba1a65a29a3215680e70d014681
LagBag71 <vitya-dolzhenkov@mail.ru>
2023-06-26 17:58:08 +0300

    hot-fix

--- Museum/views.py
+++ Museum/views.py
@@ -3,16 +3,10 @@ from .forms import ExhibitForm
 from .models import *
 
 def exhibits(request, pk):
-    exhibits = Exhibit.objects.filter(pk=pk)
+    exhibits = Exhibit.objects.filter(category=pk)
     return render(request, 'exhibitions.html', {'exhibits': exhibits})
 
 def main(request):
     category = Category.objects.all()
     events = Event.objects.all()
     return render(request, 'main.html', {'events': events, 'categories': category})
-
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
