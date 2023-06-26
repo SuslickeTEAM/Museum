26b6b907f5cd163c02e547d42929d4128c1a50de
LagBag71 <vitya-dolzhenkov@mail.ru>
2023-06-26 18:03:57 +0300

    veryfastfix

--- Museum/urls.py
+++ Museum/urls.py
@@ -5,7 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', main, name='main'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
     path('exhibits/<int:pk>', exhibits, name='exhibits'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
